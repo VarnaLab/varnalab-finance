c3b4985f41d29a2991e53bdeae85c3612c379b18
simov <simeonvelichkov@gmail.com>
2017-06-13 16:43:15 +0300

    Add forth stat

---
+++ stats/04.js
@@ -0,0 +1,32 @@
+
+// Среден месечен разход до сега (за всички месеци/години)
+// Сума на всички разходи до сега
+
+module.exports = (spend, first, current) => {
+
+  var total = result(spend)
+  var months = (Object.keys(spend).length * 12) - first - current
+
+  return {
+    total,
+    months,
+    average: Math.floor(total / months)
+  }
+}
+
+var result = (spend) =>
+  Object.keys(spend)
+    .map((year) => Object.keys(spend[year]).map((item) => spend[year][item]))
+    .reduce((all, year) => (all = all.concat(year), all), [])
+    .reduce((total, item) => (
+      total += item.reduce((sum, month) => (sum += month, sum), 0),
+      total
+    ), 0)
+
+
+// test
+if (require.main === module) {
+  var data = require('../data.json')
+  var result = module.exports(data.spend, 5, 6)
+  console.log(result)
+}
