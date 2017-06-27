93ec1d4ce0ac728d14f7675ea335e6e7b0342103
simov <simeonvelichkov@gmail.com>
2017-06-27 19:08:23 +0300

    Streamline cli flags

--- bin/index.js
+++ bin/index.js
@@ -28,49 +28,38 @@ var path = require('path')
 var env = process.env.NODE_ENV || argv.env || 'development'
 var config = require(path.resolve(process.cwd(), argv.config))[env]
 
-var dump = require('../lib/dump')(require('../config/purest'))
+var sync = require('../lib/sync')(require('../config/purest'))
 var parse = require('../lib/parse')()
 var render = require('../lib/render')
 
 
 if (argv.sync) {
   ;(async () => {
-    var sheets = await dump.spreadsheet(config.id)
-      .filter((sheet) => sheet.year === parseInt(argv.sync))
-    var files = await dump.files(config.id, sheets)
+    var sheets = await sync.spreadsheet(config.id)
+
+    // specific year
+    if (typeof argv.sync === 'number') {
+      sheets = sheets.filter((sheet) => sheet.year === argv.sync)
+    }
+
+    var files = await sync.files(config.id, sheets)
 
     sheets.forEach((sheet, index) => {
       fs.writeFileSync(
-        path.join(path.resolve(process.cwd(), argv.out, sheet.year + '.csv')),
+        path.resolve(process.cwd(), argv.out, sheet.year + '.csv'),
         files[index],
         'utf8'
       )
     })
   })()
-}
-
-else if (argv.dump) {
-  var dpath = path.resolve(process.cwd(), argv.dump)
-
-  ;(async () => {
-    var sheets = await dump.spreadsheet(config.id)
-    var files = await dump.files(config.id, sheets)
-
-    sheets.forEach((sheet, index) => {
-      fs.writeFileSync(
-        path.join(dpath, sheet.year + '.csv'),
-        files[index],
-        'utf8'
-      )
-    })
-  })()
+  .catch((err) => console.error(err))
 }
 
 else if (argv.parse) {
   parse.dump(path.resolve(process.cwd(), argv.parse))
     .then((result) => {
       fs.writeFileSync(
-        path.resolve(process.cwd(), argv.out, 'finance-dump.json'),
+        path.resolve(process.cwd(), argv.out, 'finance.json'),
         JSON.stringify(result, null, 2),
         'utf8'
       )
@@ -82,7 +71,7 @@ else if (argv.render) {
   var data = require(path.resolve(process.cwd(), argv.render))
   render(data).then((html) => {
     fs.writeFileSync(
-      path.resolve(__dirname, '../index.html'),
+      path.resolve(process.cwd(), argv.out, 'index.html'),
       '<!DOCTYPE html>\n' + html,
       'utf8')
     })
